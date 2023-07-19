e8077f4e1e9dbf8a78c37625b16e51edaa6c0eec
Felipe Gomeza <felipe@meettulip.com>
2023-07-19 15:34:20 -0500

    Configuración de AWS Serverless Framework.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import serverlessExpress from '@nestjs/platform-express';
+import serverlessHttp from 'serverless-http';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const expressApp = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  await app.init();
+
+  const serverless = serverlessHttp(expressApp);
+  module.exports.handler = serverless;
 }
 bootstrap();
