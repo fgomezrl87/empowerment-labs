886c442826986781cf7b48311979955cbb78e27c
Felipe Gomeza <felipe@meettulip.com>
2023-07-19 19:21:38 -0500

    configuring lambda ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-import * as serverless from 'aws-serverless-express';
 
 async function bootstrap() {
-  const expressApp = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-  await app.init();
-  
-  const server = serverless.createServer(expressApp);
-  
-  exports.handler = (event, context) => { 
-    console.log(`Incoming event: ${JSON.stringify(event)}`);
-    return serverless.proxy(server, event, context, 'PROMISE').promise;
-  };
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
-
 bootstrap();
