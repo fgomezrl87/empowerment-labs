db101807a7793a9720f4d44123d72fbfc9f1bf4f
Felipe Gomeza <felipe@meettulip.com>
2023-07-19 19:00:23 -0500

    Agregando paquete aws-serverless-express

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import serverlessExpress from '@nestjs/platform-express';
-import serverlessHttp from 'serverless-http';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
+import * as serverless from 'aws-serverless-express';
 
 async function bootstrap() {
   const expressApp = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
   await app.init();
-
-  const serverless = serverlessHttp(expressApp);
-  module.exports.handler = serverless;
+  
+  const server = serverless.createServer(expressApp);
+  
+  exports.handler = (event, context) => { 
+    console.log(`Incoming event: ${JSON.stringify(event)}`);
+    return serverless.proxy(server, event, context, 'PROMISE').promise;
+  };
 }
+
 bootstrap();
