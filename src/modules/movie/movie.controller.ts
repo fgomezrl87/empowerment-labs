9533724ac1338c9678e2acfbd2bbce9463a3599c
Felipe Gomeza <51337483+fgomezrl87@users.noreply.github.com>
2023-07-24 12:59:44 -0500

    Endpoint protegidos con Bearer Tokens

--- src/modules/movie/movie.controller.ts
+++ src/modules/movie/movie.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Query, Res, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Query, Res, HttpException, HttpStatus, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { MovieService } from './movie.service';
 import { Response } from 'express';
 
@@ -7,6 +8,7 @@ export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
   @Get('populate')
+  @UseGuards(AuthGuard('jwt'))
   async populateDatabase(@Res() response: Response): Promise<Response> {
     try {
       await this.movieService.populateDatabase();
@@ -20,11 +22,13 @@ export class MovieController {
   }
 
   @Get('popular')
+  @UseGuards(AuthGuard('jwt'))
   async getPopularMovies() {
     return await this.movieService.getPopularMovies();
   }
   
   @Get('search')
+  @UseGuards(AuthGuard('jwt'))
   async searchMovieByTitle(@Query('query') title: string) {
     return await this.movieService.searchMovieByTitle(title);
   }
